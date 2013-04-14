f73faa4c1e011443a693d1d7ec9f560cd598ebbe
Aubry <aubry.cholleton@epfl.ch>
2013-04-14 13:46:00 +0200

    Randomize sets

--- test.py
+++ test.py
@@ -5,28 +5,26 @@ from matplotlib import pyplot as plt
 
 def importDataFromMat(fileName):
 	tmp = spio.loadmat(fileName)
-
-	size=tmp['train_cat_s'].shape[1]
 	
-	## TODO Randomize split
-
+	size=tmp['train_cat_s'].shape[1]
+	training_set=np.random.choice(size, (2*size/3), False)
+	complete_set=np.arange(size)
+	val_set=np.setdiff1d(complete_set,training_set);
+	
 	#Training Data
-	train_cat_s=tmp['train_cat_s'][:,:(2*size/3)]
-	train_left_s=tmp['train_left_s'][:,:(2*size/3)]
-	train_right_s=tmp['train_right_s'][:,:(2*size/3)]
+	train_cat_s=tmp['train_cat_s'][:,training_set]
+	train_left_s=tmp['train_left_s'][:,training_set]
+	train_right_s=tmp['train_right_s'][:,training_set]
 
 	#Validation Data
-	val_cat_s=tmp['train_cat_s'][:,(2*size/3):]
-	val_left_s=tmp['train_left_s'][:,(2*size/3):]
-	val_right_s=tmp['train_right_s'][:,(2*size/3):]
+	val_cat_s=tmp['train_cat_s'][:,val_set]
+	val_left_s=tmp['train_left_s'][:,val_set]
+	val_right_s=tmp['train_right_s'][:,val_set]
 
 	#Test Data
 	test_cat_s=tmp['test_left_s']
 	test_left_s=tmp['test_left_s']
 	test_right_s=tmp['test_right_s']
-
-	print train_right_s.shape
-	print val_right_s.shape
 	
 	
 	#Show image
