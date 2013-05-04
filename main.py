bd80d0e365a979639bf22ccaef546596ced0d909
Aubry <aubry.cholleton@epfl.ch>
2013-05-04 22:47:04 +0200

    Rewrite forward pass with arrays instead of matrices

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
 	data = Data('miniproject_data/norb_binary.mat')
 	data.importDataFromMat()
 	data.normalizeDataset()
-	mlp = MLP(600,300,576)
+	mlp = MLP(4,5,576)
 	# mlp.forward_pass(data.train_left, data.train_right)
 	mlp.forward_pass(data.train_left, data.train_right)
 main()
--- mlp.py
+++ mlp.py
@@ -9,18 +9,16 @@ class MLP:
 		self.H2 = H2
 		self.dimension = dimension
 		
-		self.w1l = np.matrix(sp.random.normal(0, 1.0/(dimension), (H1, (dimension+1))))
-		self.w1r = np.matrix(sp.random.normal(0, 1.0/(dimension), (H1, (dimension+1))))
-		self.w2l = np.matrix(sp.random.normal(0, 1.0/H1, (H2, H1+1)))
-		self.w2lr = np.matrix(sp.random.normal(0, 1.0/(2*H1), (H2, (2*H1)+1)))
-		self.w2r = np.matrix(sp.random.normal(0, 1.0/H1, (H2, H1+1)))
-		self.w3 = np.matrix(sp.random.normal(0, 1.0/H2, (1, H2+1)))
+		self.w1l = sp.random.normal(0, 1.0/(dimension), (H1, (dimension+1)))
+		self.w1r = sp.random.normal(0, 1.0/(dimension), (H1, (dimension+1)))
+		self.w2l = sp.random.normal(0, 1.0/H1, (H2, H1+1))
+		self.w2lr = sp.random.normal(0, 1.0/(2*H1), (H2, (2*H1)+1))
+		self.w2r = sp.random.normal(0, 1.0/H1, (H2, H1+1))
+		self.w3 = sp.random.normal(0, 1.0/H2, (1, H2+1))
 		
-		pprint(self.w1l)
+
 
 	def forward_pass(self, xL, xR):
-		xL=np.matrix(xL)
-		xR=np.matrix(xR)
 			
 		# First Layer
 		bs = sp.ones((1,xL.shape[1]), dtype=float)
@@ -28,8 +26,8 @@ class MLP:
 		xLb = sp.vstack([xL, bs])
 		xRb = sp.vstack([xR, bs])
 
-		a1L = self.w1l * xLb
-		a1R = self.w1r * xRb
+		a1L = sp.dot(self.w1l, xLb)
+		a1R = sp.dot(self.w1r, xRb)
 		
 		z1L = sp.tanh(a1L)
 		z1R = sp.tanh(a1R)
@@ -41,15 +39,16 @@ class MLP:
 		z1LRb = sp.vstack([z1L, z1R, bs])
 		z1Rb = sp.vstack([z1R, bs])
 		
-		a2L = self.w2l * z1Lb
-		a2LR = self.w2lr * z1LRb
-		a2R = self.w2r* z1Rb
+		a2L = sp.dot(self.w2l, z1Lb)
+		a2LR = sp.dot(self.w2lr, z1LRb)
+		a2R = sp.dot(self.w2r, z1Rb)
 		
-		z2 = sp.multiply(a2LR,sp.multiply((self.sigmoid(a2L)),(self.sigmoid(a2R))))
+		z2 = a2LR*self.sigmoid(a2L)*self.sigmoid(a2R)
+		
 		# Third Layer
 		z2b = sp.vstack([z2, bs])
 		
-		a3 = self.w3 * z2b
+		a3 = sp.dot(self.w3, z2b)
 		
 		print a3.shape
 		pprint(a3)
