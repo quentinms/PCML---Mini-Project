eb56eab527ad6b83611005cf2dbdf07f8dbfca5b
aubry74 <aubry.cholleton@epfl.ch>
2013-06-06 11:50:40 +0200

    Add model evaluation

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from train_validate import TrainerValidator
 from data import Data
+from model_evaluation import ModelEvaluation
 from test import Test
+import matplotlib.pyplot as plt
 
 
 def main():
 
 	k=2
-
+	"""
 	data = Data(k, 0, 0)
 	data.importDataFromMat()
 	data.normalize()
+	"""
 
+	evalModel = ModelEvaluation()
+	evalModel.findH1H2(0.001, 0.1, 1, k)
+	"""
 	train = TrainerValidator(k, 2, 20, 50, 0.001, 0.1, 1, data)
 	train.trainAndClassify()
 	train.plotResults()
@@ -19,6 +25,8 @@ def main():
 	test.classify()
 	#test.examples()
 	test.plot_confusion_matrix()
+	"""
+	plt.show()
 
 
 
