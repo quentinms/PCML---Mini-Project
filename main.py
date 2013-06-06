8d524b5407bfb640fd49133f849c589201784b93
quentinms <quentinmazarssimon@gmail.com>
2013-06-06 15:53:10 +0200

    find Crossvalidation parameters

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from data import Data
 from model_evaluation import ModelEvaluation
 from test import Test
 import matplotlib.pyplot as plt
+import scipy as sp
+
 
 
 def main():
 
-	k=2
+	k=5
 	"""
 	data = Data(k, 0, 0)
 	data.importDataFromMat()
@@ -15,7 +17,11 @@ def main():
 	"""
 
 	evalModel = ModelEvaluation()
-	evalModel.findH1H2(0.001, 0.1, 1, k)
+	#evalModel.findH1H2(0.001, 0.1, 1, k)
+
+	v_parameters = sp.array([0, 1, 2, 4, 8, 16, 32, 64, 128, 259])
+	evalModel.findV(v_parameters, 2, k)
+
 	"""
 	train = TrainerValidator(k, 2, 20, 50, 0.001, 0.1, 1, data)
 	train.trainAndClassify()
